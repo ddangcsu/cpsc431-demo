f4efbf9b02a4a4ef1fadbf9fa7e5b5884276e6f1
David Dang <ddang.sw@gmail.com>
2016-04-25 15:06:55 -0700

    Change bootstrap to use the free united color scheme

--- application/views/header.php
+++ application/views/header.php
@@ -7,8 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?=base_url("lib/bootstrap/css/bootstrap-theme.min.css")?>" type="text/css">
-    <link rel="stylesheet" href="<?=base_url("lib/bootstrap/css/bootstrap.min.css") ?>" type="text/css">
+    <link rel="stylesheet" href="<?=base_url("lib/bootstrap/css/bootstrap.united.min.css") ?>" type="text/css">
     <link rel="stylesheet" href="<?=base_url("css/prism.css")?>" type="text/css">
     <link rel="stylesheet" href="<?=base_url("css/style.css") ?>" type="text/css">
 
